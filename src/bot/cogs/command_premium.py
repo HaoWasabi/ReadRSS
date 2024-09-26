c69321daccdbdf2730fc2c806ce4317c85285f64
HaoWasabi <truonggiahao24@gmail.com>
2024-09-26 19:49:01 +0700

    Bản cho tester v2, `_premium` giờ có thể  dùng đc ở DMChannel

--- src/bot/cogs/command_premium.py
+++ src/bot/cogs/command_premium.py
@@ -1,21 +1,20 @@
 from email import message
 from tkinter.messagebox import NO
-from nextcord import ButtonStyle, Emoji, PartialEmoji
-import logging, os , datetime, mbbank, re
+from nextcord import ButtonStyle, Emoji, PartialEmoji, User
+import logging, os, datetime, mbbank, re
 import sys
 from typing import Optional
 from nextcord.ext import commands, tasks
 from nextcord.ext.commands import Context
 import nextcord
-from nextcord import Interaction, SlashOption, TextChannel
-from nextcord.ui import View, Button, button
+from nextcord import Interaction, TextChannel
+from nextcord.ui import View, Button
 
 from ..utils.datetime_format import datetime_from_string
 
+from ..DTO.user_dto import UserDTO
 from ..DTO.premium_dto import PremiumDTO
-
 from ..GUI.button_of_premium import ButtonOfPayment
-
 from ..DTO.user_premium_dto import UserPremiumDTO
 from ..DTO.transaction_history_dto import TransactionHistoryDTO
 from ..DTO.qr_code_pay_dto import QrPayCodeDTO
@@ -24,18 +23,19 @@ from ..BLL.premium_bll import PremiumBLL
 from ..BLL.user_premium_bll import UserPremiumBLL
 from ..BLL.transaction_history_bll import TransactionHistoryBLL
 from ..BLL.qr_pay_code_bll import QrPayCodeBLL
-
+from ..BLL.user_bll import UserBLL
 from ..GUI.embed_custom import EmbedCustom
 
 from ..utils.commands_cog import CommandsCog
 from ..utils.create_qr_payment import QRGenerator
+
 
 logger = logging.getLogger('paying')
 
 class CommandPaying(CommandsCog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.mb = mbbank.MBBankAsync( username=os.getenv('BANK_USER_NAME'), password=os.getenv('BANK_PASSWORD'))
+        self.mb = mbbank.MBBankAsync(username=os.getenv('BANK_USER_NAME'), password=os.getenv('BANK_PASSWORD'))
         self.start_time: datetime.datetime = datetime.datetime.now()
         
     @tasks.loop(seconds=5)
@@ -43,55 +43,49 @@ class CommandPaying(CommandsCog):
         qr_pay_code_bll = QrPayCodeBLL()
         all_qr = qr_pay_code_bll.get_all_qr_pay_code()
         for i in all_qr:
-            denta = datetime.datetime.now() - i.date_created # type: ignore
-            # kiểm tra hết hạng của mã qr
-            # ở đây tôi để 3p
+            denta = datetime.datetime.now() - i.date_created  # type: ignore
             if denta > datetime.timedelta(minutes=3) and not i.is_success:
                 qr_pay_code_bll.delete_qr_pay_by_id(i.qr_code)
-                channel = self.bot.get_channel(int(i.channel_id))
+                channel = self.bot.get_channel(int(i.channel_id)) or self.bot.fetch_user(int(i.channel_id))
                 
-                if (channel is None): 
-                    logger.warning('không tìm thấy channel')
+                if channel is None: 
+                    logger.warning('Không tìm thấy channel')
                     return
                 
                 if isinstance(channel, TextChannel):
                     message = await channel.fetch_message(int(i.message_id))
-                    await message.edit(content = 'qr đã hết hạn', embed=None)
+                    await message.edit(content='QR đã hết hạn', embed=None)
     
     async def get_bank_history(self):
         from_day = datetime.datetime.now()
         an = os.getenv('BANK_USER_NAME')
-        if (an is None):
-            logger.error('không có BANK_USER_NAME')
+        if an is None:
+            logger.error('Không có BANK_USER_NAME')
             sys.exit(0)
             
         history = await self.mb.getTransactionAccountHistory(accountNo=an, from_date=from_day - datetime.timedelta(days=2), to_date=from_day)
         for i in history['transactionHistoryList']:
-            i: dict
             transaction_id = i.get('refNo', '')
             transactionDate = i.get('transactionDate', '')
             creditAmount = int(i.get('creditAmount', ''))
             currency = i.get('currency', '')
-            description : str = i.get('description', '')
+            description: str = i.get('description', '')
             
             transaction_history_bll = TransactionHistoryBLL()
             qr_pay_code_bll = QrPayCodeBLL()
-            user_premium_bll = UserPremiumBLL()
+            user_bll = UserBLL()
             premium_bll = PremiumBLL()
             
-            for qrcode in re.findall("T\\d{20}T" , description.replace(' ', '').upper()):
+            for qrcode in re.findall("T\\d{20}T", description.replace(' ', '').upper()):
                 qr = qr_pay_code_bll.get_qr_pay_code_by_qr_code(qrcode.replace("T", ""))
-                
                 
                 if qr is None:
                     continue
                 
-                # NOTE lưu những dao dịnh đã được sử lý
                 transaction = transaction_history_bll.get_transaction_history_by_id(transaction_id)
                 if transaction is not None:
                     break
                 
-                    
                 transaction_history_bll.insert_transaction_history(TransactionHistoryDTO(
                     transaction_id, 
                     qrcode,
@@ -101,80 +95,78 @@ class CommandPaying(CommandsCog):
                     currency
                 ))
                 
-                # NOTE sác nhận là qr được chuyển tiền thành công
                 qr.is_success = True
                 qr_pay_code_bll.insert_qr_pay_code(qr)
-                 
-            
-                await self.pay_success(qr)
+                user_dto = user_bll.get_user_by_user_id(qr.user_id)
+                premium_dto = premium_bll.get_premium_by_id(qr.premium_id)
+                if premium_dto is None:
+                    logger.error(f'PremiumDTO không tìm thấy cho premium_id: {qr.premium_id}')
+                    continue
+                if user_dto is None:
+                    logger.error(f'UserDTO không tìm thấy cho user_id: {qr.user_id}')
+                    continue
+                user_premium_dto = UserPremiumDTO(user_dto, premium_dto, datetime.datetime.now())
+                await self.pay_success(qr, user_premium_dto)
     
-    async def pay_success(self, qr: QrPayCodeDTO):
-        channel = self.bot.get_channel(int(qr.channel_id))
-
-        if (channel is None): 
-            logger.warning('không tìm thấy channel')
-            return
-        
-        if isinstance(channel, TextChannel):
-            message = await channel.fetch_message(int(qr.message_id))
-            await message.edit(content = 'Thanh toán thành công', embed=None)
-                         
+    async def pay_success(self, qr: QrPayCodeDTO, user_premium_dto: UserPremiumDTO):
+        try:
+            channel = self.bot.get_channel(int(qr.channel_id)) or self.bot.fetch_user(int(qr.channel_id))
+            user_premium_bll = UserPremiumBLL()
+            if channel is None: 
+                logger.warning('Không tìm thấy channel')
+                return
+            
+            if isinstance(channel, TextChannel):
+                user_premium_bll.insert_user_premium(user_premium_dto)
+                message = await channel.fetch_message(int(qr.message_id))
+                await message.edit(content='Thanh toán thành công', embed=None)
+        except Exception as e:
+            logger.error(f'Không thể thanh toán: {e}')
+                             
     @tasks.loop(seconds=30)
     async def start_listener_bank_history(self):
-        # thời gian lấy lịch sử
-        # chỉ khi có giao dịch sảu ra mới lấy lịch sử
-        # tôi để 5p
         if datetime.datetime.now() - self.start_time > datetime.timedelta(minutes=5):
             self.start_listener_bank_history.stop()
         
-        # print('aaaaaaaaaaaaa')
         await self.get_bank_history() 
 
     @commands.Cog.listener()
     async def on_ready(self):
-        logger.info('check qr code start')
+        logger.info('Kiểm tra mã QR bắt đầu')
         if not self.check_qr_code.is_running():
             self.check_qr_code.start()
               
     @commands.command(name="premium")
     async def pay(self, ctx: Context):
         '''Nạp để mở gói premium'''
-        # if await self.is_dm_channel(ctx): 
-        #     return
-        
-        # if not await self.is_server_owner(ctx=ctx):
-        #     return
+        user_dto = UserDTO(str(ctx.author.id), ctx.author.name)
+        UserBLL().insert_user(user_dto)
         
         embed_text = EmbedCustom(
-        id_server=str(ctx.guild.id) if ctx.guild else "DM",
-        title="Nạp để mở gói premium",
-        description="Gói premium mang đến nhiều tính năng hơn. ở đây là một số gói cơ bản:")
+            id_server=str(ctx.guild.id) if ctx.guild else str(ctx.author.id),
+            title="Nạp để mở gói premium",
+            description="Gói premium mang đến nhiều tính năng hơn. Đây là một số gói cơ bản:"
+        )
         
         premium_bll = PremiumBLL()
         buttons = View()
         
         cls = self
         
-        class goiButton(Button):
+        class GóiButton(Button):
             def __init__(self, pre: PremiumDTO):
                 super().__init__(label=pre.get_premium_name())
                 self.pre = pre
                 
             async def callback(self, interaction: Interaction):
-                
                 embed_text = EmbedCustom(
-                    id_server=str(ctx.guild.id) if ctx.guild else "DM",
+                    id_server=str(ctx.guild.id) if ctx.guild else str(ctx.author.id),
                     title="Nạp để mở gói premium",
-                    description="Gói premium mang đến nhiều tính năng hơn")
-
-
+                    description="Gói premium mang đến nhiều tính năng hơn"
+                )
 
                 embed_text.add_field(name="Ngân hàng MB-Bank", value=os.getenv('BANK_USER_NAME'), inline=False)
-                embed_text.add_field(name="Số Tiền:", value=f"{self.pre.get_price()}đ", inline=False)
-
-                if (ctx.guild is None or ctx.channel is None):
-                    await ctx.send('có gì đó lạ lắm')
-                    return
+                embed_text.add_field(name="Số tiền:", value=f"{self.pre.get_price()}đ", inline=False)
                 
                 qr_id = QRGenerator.generator_id()
                 embed_text.add_field(name="Nội dung:", value=f"T{qr_id}T", inline=False)
@@ -183,25 +175,36 @@ class CommandPaying(CommandsCog):
                 message = await interaction.edit(content='', embed=embed_text, view=None)
                 
                 if message is None:
-                    await ctx.send('có gì đó lạ lắm')
+                    await ctx.send('Có gì đó lạ lắm')
                     return
-                qr = QrPayCodeDTO(qr_id, str(ctx.author.id), str(message.channel.id), self.pre.get_premium_id(), str(message.id), datetime.datetime.now(), False) # type: ignore
+                
+                qr = QrPayCodeDTO(
+                    qr_id, 
+                    str(ctx.author.id), 
+                    str(message.channel.id) if message.channel.id else str(ctx.author.id), 
+                    self.pre.get_premium_id(),  # type: ignore
+                    str(message.id), 
+                    datetime.datetime.now(), 
+                    False
+                )
                 
                 qr_pay_code_bll = QrPayCodeBLL()
                 qr_pay_code_bll.insert_qr_pay_code(qr)
+                
                 if not cls.start_listener_bank_history.is_running():
                     cls.start_listener_bank_history.start()
                 cls.start_time = datetime.datetime.now()
-                
-        
-        for index, pre in enumerate(premium_bll.get_all_premiums()):
-            embed_text.add_field(name=pre.get_premium_name(), value=f"""{pre.get_description()}\ngiá: {pre.get_price()}\nthời hạn: {pre.get_duration()}""", inline=False)
+
+        for pre in premium_bll.get_all_premiums():
+            embed_text.add_field(
+                name=pre.get_premium_name(), 
+                value=f"{pre.get_description()}\nGiá: {pre.get_price()}đ\nThời hạn: {pre.get_duration()}",
+                inline=False
+            )
                     
-            buttons.add_item(goiButton(pre))
+            buttons.add_item(GóiButton(pre))
         
         await ctx.send(embed=embed_text, view=buttons)
-        
 
 async def setup(bot: commands.Bot):
-    # NOTE: add_cog là một funstion bình thường không phải là async funstion
     bot.add_cog(CommandPaying(bot))
